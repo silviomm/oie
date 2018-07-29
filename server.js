d83b5ae48c760f74acd751377233bf2219097e66
silviomm <silviommattos@gmail.com>
2018-07-29 16:29:42 -0300

    listen ip 0

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use(bodyParser.json());
 
 require('./api')(app);
 
-app.listen(8080, "127.0.0.1", () => 
+app.listen(8080, "0.0.0.0", () => 
     console.log(`Servidor rodando em http://${ip}:8080`));
